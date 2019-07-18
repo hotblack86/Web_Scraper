0725b31fea4351218bcf54396ff4dad1bd267ff2
James Hotblack <james.c.hotblack@gmail.com>
2019-07-18 10:20:09 +0100

    send_mail function implemented. Updated readme and added code comments.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 **Website Scraper**
 
-A Python app that scrapes prices from websites and sends user an email when price drops below a certain level
+A Python app that scrapes prices from websites and sends user an email when price drops below a certain level.
+
+Configured to for Amazon UK and Gmail.
+
+Replace < code > with your details.
--- scraper.py
+++ scraper.py
@@ -1,26 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 # The URL of the Amazon UK page you wish to scrape.
-URL = 'https://www.amazon.co.uk/LG-49UK6300PLB-49-Inch-Smart-Freeview/dp/B07BN3RCXW?ref_=Oct_MWishedForC_560864_0&pf_rd_r=YRDE2AH6NBBPWSVNFD9G&pf_rd_p=b0d77b0a-c5e0-5343-8b66-cd36853c24c1&pf_rd_s=merchandised-search-10&pf_rd_t=101&pf_rd_i=560864&pf_rd_m=A3P5ROKL5A1OLE'
+URL = '<Amazon-URL>'
 
 # Google 'my user agent' to find your own.
 headers = {"User-Agent":
-'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+'<Your-User-Agent>'
 }
 
-def check_price()
+def check_price():
   page = requests.get(URL, headers=headers)
   soup = BeautifulSoup(page.content, 'html.parser')
 
-  title = soup.find(id="title").get_text()
-  price = soup.find(id="priceblock_ourprice").get_text()
+  # id may be different for your chosen product.
+  # 'Inspect' title and price and replace with correct id if needed.
+  title = soup.find(id="productTitle").get_text()
+  price = soup.find(id="priceblock_dealprice").get_text()
   converted_price = float(price[1:4])
 
-  if(converted_price < 300)
+  if(converted_price < 300):
     send_mail()
 
-  print(title.strip())
-  print(converted_price)
 
-def send_mail()  
+def send_mail():
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.ehlo()
+  server.starttls()
+  server.ehlo()
+
+  server.login('<Your-Gmail-Account>', '<Your-Gmail-Password>')
+
+  subject = 'Price Drop!'
+  body = 'Check the link: <Amazon-URL>'
+
+  msg = f"Subject: {subject}\n\n{body} "
+
+  server.sendmail(
+    '<Your-Gmail-Account>',
+    '<Recipient-Email-Account>,
+    msg
+  )
+
+  print('Email Sent!')
+
+  server.quit()
+
+  check_price()
